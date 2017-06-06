157e685a3200aa46781aeb033f90739042cdb524
Maarten <m.dohmen@student.fontys.nl>
2017-06-06 10:45:05 +0200

    Added unit tests and fixed table customerpage

---
+++ Testing/CrewActions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fun_Killerapp_S2;
+using System.Collections.Generic;
+using Fun_Killerapp_S2.Object;
+using System.Linq;
+
+namespace Testing
+{
+    [TestClass]
+    public class CrewActions
+    {
+        [TestMethod]
+        public void TestGetAllOrders()
+        {
+            //kijk of je alle orders kan opvragen
+            CrewOverview cr = new CrewOverview();
+            Assert.IsNotNull(cr.GetAllOrders());
+        }
+
+        [TestMethod]
+        public void TestSaveLatestsLogin()
+        {
+            //save nieuwe laatste login-tijd
+            CrewOverview cr = new CrewOverview();
+            cr.Savelasttimelogin(DateTime.Now, 0);
+        }
+
+        [TestMethod]
+        public void TestSaveDiscount()
+        {
+            //Kijk of je een Discount kan Saven (even tijdelijk op alle producten)
+            CrewOverview cr = new CrewOverview();
+            cr.SaveDiscounts(50, DateTime.Now, "Nieuwe korting", cr.GetAllProducts());
+        }
+
+        [TestMethod]
+        public void TestDeleteDiscount()
+        {
+            //kijk of je de hiervoor genoemde discount weer kan verwijderen
+            CrewOverview cr = new CrewOverview();
+            List<Discount> alldiscounts = cr.GetAllDiscounts().Cast<Discount>().ToList();
+            Discount latest = alldiscounts.Last();
+            cr.DeleteDiscount(latest.DiscountID);
+        }
+    }
+}
---
+++ Testing/CustomerActions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fun_Killerapp_S2;
+using Fun_Killerapp_S2.Object;
+
+namespace Testing
+{
+    [TestClass]
+    public class CustomerActions
+    {
+        [TestMethod]
+        public void TestGetAllProducts()
+        {
+            //laat zien dat alle producten worden ingeladen
+            CustomerOverview cu = new CustomerOverview();
+            Assert.IsNotNull(cu.GetAllProducts());
+        }
+
+        [TestMethod]
+        public void TestAddProduct()
+        {
+            //laat zien dat je kan inloggen
+            Product nieuw = new Product(15, new Supplier(20, "V&D", "Roermond"), 30, "Axe cool", Fun_Killerapp_S2.Object.Enum.ProductCatagorie.Productsoort.Spellen, 30);
+        }
+    }
+}
